7cf076127dc55f0425c618885923696104d3c19a
Erik Bjäreholt <erik@bjareho.lt>
2019-07-17 12:24:59 +0200

    added logging of savings to TransactionMuxer test

--- test/TransactionMuxer.test.js
+++ test/TransactionMuxer.test.js
@@ -2,23 +2,32 @@
 
 const assert = require('assert');
 
-const TransactionMuxer = artifacts.require("TransactionMuxer");
+const TransactionMuxer = artifacts.require('TransactionMuxer');
 
-contract('TransactionMuxer', async (accounts) => {
-    let addr_empty = "0x0000000000000000000000000000000000000000";
+contract('TransactionMuxer', async accounts => {
+    let addr_empty = '0x0000000000000000000000000000000000000000';
     let addr_sender = accounts[0];
-    let receivers = accounts.slice(1, 6);
+    let receivers = accounts.slice(1, 9);
 
-    it("send", async () => {
+    it('send', async () => {
         let muxer = await TransactionMuxer.deployed();
         let totalAmount = 1000;
-        let amounts = [
-            Math.round(0.3 * totalAmount),
-            Math.round(0.3 * totalAmount),
-            Math.round(0.2 * totalAmount),
-            Math.round(0.1 * totalAmount),
-            Math.round(0.1 * totalAmount),
-        ];
-        await muxer.transferMany(receivers, amounts, {from: addr_sender, value: totalAmount})
-    })
+
+        let amounts = [];
+        for (var i = 0; i < receivers.length; i++) {
+            amounts.push(totalAmount / receivers.length);
+        }
+
+        let r = await muxer.transferMany(receivers, amounts, {
+            from: addr_sender,
+            value: totalAmount,
+        });
+        let gasUsed = r.receipt.gasUsed;
+        let savings = 1 - gasUsed / (21000 * receivers.length);
+        console.log(
+            `Used ${gasUsed} gas for ${
+                receivers.length
+            } transactions, saving ${Math.round(savings * 100)}%`
+        );
+    });
 });
